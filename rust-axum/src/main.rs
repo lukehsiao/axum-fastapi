15323d54b321e6e239ccdf97eb2f932ea6d95d83
Luke Hsiao <luke@hsiao.dev>
2023-09-08 17:09:23 -0600

    feat(axum): dead simple axum server with sqlx, postgres

---
+++ rust-axum/src/main.rs
@@ -0,0 +1,115 @@
+//! Example of application using <https://github.com/launchbadge/sqlx>
+//!
+//! Run with
+//!
+//! ```not_rust
+//! cargo run -p example-sqlx-postgres
+//! ```
+//!
+//! Test with curl:
+//!
+//! ```not_rust
+//! curl 127.0.0.1:3000
+//! curl -X POST 127.0.0.1:3000
+//! ```
+
+use std::{net::SocketAddr, time::Duration};
+
+use axum::{
+    async_trait,
+    extract::{FromRef, FromRequestParts},
+    http::{request::Parts, StatusCode},
+    routing::get,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use sqlx::{
+    postgres::{PgPool, PgPoolOptions},
+    FromRow,
+};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+/// A struct representing a Document's JSON.
+#[derive(Debug, Clone, Deserialize, Serialize, FromRow)]
+pub struct User {
+    user_id: String,
+    username: String,
+    email: String,
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "example_tokio_postgres=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let db_connection_str = std::env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "postgres://postgres:password@localhost".to_string());
+
+    // set up connection pool
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&db_connection_str)
+        .await
+        .expect("can't connect to database");
+
+    // build our application with some routes
+    let app = Router::new().route("/", get(read_users)).with_state(pool);
+
+    // run it with hyper
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// we can also write a custom extractor that grabs a connection from the pool
+// which setup is appropriate depends on your application
+struct DatabaseConnection(sqlx::pool::PoolConnection<sqlx::Postgres>);
+
+#[async_trait]
+impl<S> FromRequestParts<S> for DatabaseConnection
+where
+    PgPool: FromRef<S>,
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, String);
+
+    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let pool = PgPool::from_ref(state);
+
+        let conn = pool.acquire().await.map_err(internal_error)?;
+
+        Ok(Self(conn))
+    }
+}
+
+async fn read_users(
+    DatabaseConnection(mut conn): DatabaseConnection,
+) -> Result<(StatusCode, Json<Vec<User>>), (StatusCode, String)> {
+    let users: Vec<User> = sqlx::query_as!(
+        User,
+        "SELECT user_id, username, email FROM \"user\" ORDER BY user_id"
+    )
+    .fetch_all(&mut *conn)
+    .await
+    .map_err(internal_error)?;
+
+    Ok((StatusCode::OK, Json(users[..50].to_vec())))
+}
+
+/// Utility function for mapping any error into a `500 Internal Server Error`
+/// response.
+fn internal_error<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+}
